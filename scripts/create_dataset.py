16efd2a206025e13d7964937569f95b1d49ca13f
otenim <snake_istobelieve@keio.jp>
2023-11-10 15:53:27 +0900

    update

--- scripts/create_dataset.py
+++ scripts/create_dataset.py
@@ -2,6 +2,7 @@ import pandas as pd
 import click
 import json
 from sklearn.preprocessing import LabelEncoder
+from scraper.spiders.constants import *
 
 
 @click.command()
@@ -18,7 +19,38 @@ def main(jsonfile: str):
     df_fighters = pd.json_normalize(
         jsondata,
         errors="ignore",
+    ).astype(
+        {
+            "url": pd.StringDtype(),
+            "id": pd.StringDtype(),
+            "name": pd.StringDtype(),
+            "nickname": pd.StringDtype(),
+            "nationality": pd.CategoricalDtype(),
+            "weight_class": pd.CategoricalDtype(),
+            "career_earnings": pd.Int64Dtype(),
+            "height": pd.Float32Dtype(),
+            "reach": pd.Float32Dtype(),
+            "head_coach": pd.StringDtype(),
+            "last_weigh_in": pd.Float32Dtype(),
+            "affiliation.url": pd.StringDtype(),
+            "affiliation.id": pd.StringDtype(),
+            "affiliation.name": pd.StringDtype(),
+            "college": pd.StringDtype(),
+        }
     )
+    df_fighters = df_fighters.drop(["results"], axis=1)
+    df_fighters["date_of_birth"] = pd.to_datetime(
+        df_fighters["date_of_birth"], format="%Y-%m-%d"
+    )
+    df_fighters["nationality"] = (
+        df_fighters["nationality"].cat.add_categories(["unknown"]).fillna("unknown")
+    )
+    df_fighters["height"] = df_fighters.groupby("weight_class", observed=True)[
+        "height"
+    ].transform(lambda x: x.fillna(x.mean()))
+    df_fighters["reach"] = df_fighters.groupby("weight_class", observed=True)[
+        "reach"
+    ].transform(lambda x: x.fillna(x.mean()))
     df_fighters.info()
 
 
