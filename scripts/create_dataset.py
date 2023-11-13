5209db76aeb6d2fbfff86b692a50406076da33e4
otenim <snake_istobelieve@keio.jp>
2023-11-13 18:50:48 +0900

    update

--- scripts/create_dataset.py
+++ scripts/create_dataset.py
@@ -15,95 +15,69 @@ def main(jsonfile: str):
     with open(jsonfile) as fp:
         jsondata = json.load(fp)
 
-    # Dataframe of fighters
-    df_fighters = pd.json_normalize(
-        jsondata,
-        errors="ignore",
-    ).astype(
-        {
-            "url": pd.StringDtype(),
-            "id": pd.StringDtype(),
-            "name": pd.StringDtype(),
-            "nickname": pd.StringDtype(),
-            "nationality": pd.CategoricalDtype(),
-            "weight_class": pd.CategoricalDtype(),
-            "career_earnings": pd.Int64Dtype(),
-            "height": pd.Float32Dtype(),
-            "reach": pd.Float32Dtype(),
-            "head_coach": pd.StringDtype(),
-            "last_weigh_in": pd.Float32Dtype(),
-            "affiliation.url": pd.StringDtype(),
-            "affiliation.id": pd.StringDtype(),
-            "affiliation.name": pd.StringDtype(),
-            "college": pd.StringDtype(),
-        }
-    )
-    df_fighters = df_fighters.drop(["results"], axis=1)
-    df_fighters["date_of_birth"] = pd.to_datetime(
-        df_fighters["date_of_birth"], format="%Y-%m-%d"
-    )
-    df_fighters["nationality"] = (
-        df_fighters["nationality"].cat.add_categories(["unknown"]).fillna("unknown")
-    )
-    df_fighters["height"] = df_fighters.groupby("weight_class", observed=True)[
-        "height"
-    ].transform(lambda x: x.fillna(x.mean()))
-    df_fighters["reach"] = df_fighters.groupby("weight_class", observed=True)[
-        "reach"
-    ].transform(lambda x: x.fillna(x.mean()))
-    target_columns = [
-        col for col in df_fighters.columns if col.startswith("career_record")
-    ]
-    df_fighters[target_columns] = df_fighters[target_columns].astype(pd.Int16Dtype())
-    df_fighters[target_columns] = df_fighters[target_columns].fillna(0)
-    df_fighters.info()
+    # Load dataframe of fighter data
+    df_fighters = pd.json_normalize(jsondata).drop(["results"], axis="columns")
 
-    # Dataframe of results
-    df_results = (
-        pd.json_normalize(
-            jsondata,
-            "results",
-            ["id"],
-            errors="ignore",
-        )
-        .astype(
-            {
-                "division": pd.CategoricalDtype(),
-                "status": pd.CategoricalDtype(),
-                "billing": pd.CategoricalDtype(),
-                "opponent.name": pd.StringDtype(),
-                "opponent.url": pd.StringDtype(),
-                "opponent.id": pd.StringDtype(),
-                "opponent.record.w": pd.Int16Dtype(),
-                "opponent.record.l": pd.Int16Dtype(),
-                "opponent.record.d": pd.Int16Dtype(),
-                "record.w": pd.Int16Dtype(),
-                "record.l": pd.Int16Dtype(),
-                "record.d": pd.Int16Dtype(),
-                "ended_by.type": pd.CategoricalDtype(),
-                "ended_by.detail": pd.CategoricalDtype(),
-                "weight.is_open": pd.BooleanDtype(),
-                "weight.is_catch": pd.BooleanDtype(),
-                "weight.class": pd.CategoricalDtype(),
-                "weight.limit": pd.Float32Dtype(),
-                "weight.weigh_in": pd.Float32Dtype(),
-                "ended_at.round": pd.Int16Dtype(),
-                "promotion.url": pd.StringDtype(),
-                "promotion.id": pd.StringDtype(),
-                "age": pd.Int16Dtype(),
-                "odds": pd.Float32Dtype(),
-                "title_info.name": pd.StringDtype(),
-                "title_info.type": pd.CategoricalDtype(),
-                "referee": pd.CategoricalDtype(),
-                "title_info.as": pd.CategoricalDtype(),
-                "id": pd.StringDtype(),
-            }
-        )
-        .rename(columns={"id": "fighter_id"})
-    )
-    df_results["date"] = pd.to_datetime(df_results["date"], format="%Y-%m-%d")
-    df_results["ended_at.time"] = pd.to_timedelta(df_results["ended_at.time"])
-    df_results.info()
+    # Load dataframe of result data
+    df_results = pd.json_normalize(jsondata, record_path="results", meta=["id"])
+
+    # Merge fighters & results
+    df = pd.merge(df_fighters, df_results, on="id")
+
+    # Convert data type
+    dtypes = {
+        "url": pd.StringDtype(),
+        "id": pd.StringDtype(),
+        "name": pd.StringDtype(),
+        "nationality": pd.CategoricalDtype(),
+        "weight_class": pd.CategoricalDtype(),
+        "career_earnings": pd.Int32Dtype(),
+        "nickname": pd.StringDtype(),
+        "height": pd.Float32Dtype(),
+        "reach": pd.Float32Dtype(),
+        "last_weigh_in": pd.Float32Dtype(),
+        "affiliation.url": pd.StringDtype(),
+        "affiliation.id": pd.StringDtype(),
+        "affiliation.name": pd.StringDtype(),
+        "head_coach": pd.StringDtype(),
+        "college": pd.StringDtype(),
+        "division": pd.CategoricalDtype(),
+        "status": pd.CategoricalDtype(),
+        "age": pd.Int16Dtype(),
+        "billing": pd.CategoricalDtype(),
+        "referee": pd.StringDtype(),
+        "promotion.url": pd.StringDtype(),
+        "promotion.id": pd.StringDtype(),
+        "ended_by.type": pd.CategoricalDtype(),
+        "ended_by.detail": pd.StringDtype(),
+        "ended_at.round": pd.Int16Dtype(),
+        "weight.is_open": pd.BooleanDtype(),
+        "weight.is_catch": pd.BooleanDtype(),
+        "weight.class": pd.CategoricalDtype(),
+        "weight.limit": pd.Float32Dtype(),
+        "weight.weigh_in": pd.Float32Dtype(),
+        "opponent.name": pd.StringDtype(),
+        "opponent.id": pd.StringDtype(),
+        "opponent.url": pd.StringDtype(),
+        "opponent.record.w": pd.Int16Dtype(),
+        "opponent.record.l": pd.Int16Dtype(),
+        "opponent.record.d": pd.Int16Dtype(),
+        "record.w": pd.Int16Dtype(),
+        "record.l": pd.Int16Dtype(),
+        "record.d": pd.Int16Dtype(),
+        "title_info.name": pd.StringDtype(),
+        "title_info.as": pd.CategoricalDtype(),
+        "title_info.type": pd.CategoricalDtype(),
+        "odds": pd.Float32Dtype(),
+    }
+    for c in df.columns:
+        if c.startswith("career_record"):
+            dtypes[c] = pd.Int16Dtype()
+    df = df.astype(dtypes)
+    df["date_of_birth"] = pd.to_datetime(df["date_of_birth"], format="%Y-%m-%d")
+    df["date"] = pd.to_datetime(df["date"], format="%Y-%m-%d")
+    df["ended_at.time"] = pd.to_timedelta(df["ended_at.time"])
+    df.info(verbose=True, show_counts=True)
 
 
 if __name__ == "__main__":
